fa5d1641832e9fa2e5512175ac02fba5d8a35d5b
Melvin Carvalho <melvincarvalho@gmail.com>
2019-06-11 19:04:04 +0200

    add images

--- src/components/NamedNode.jsx
+++ src/components/NamedNode.jsx
@@ -2,5 +2,11 @@ import React from 'react'
 
 /** Displays a NamedNode by value. */
 export default function NamedNode ({value, ...props}) {
-  return <a href={value} alt=''>{value}</a>
+  var contentType
+  if (/.png$|.jpg$/.test(value)) {
+    contentType = 'image'
+    return <img src={value} alt='' />
+  } else {
+    return <a href={value} alt=''>{value}</a>
+  }
 }
