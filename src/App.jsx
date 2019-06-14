217d73d6b8d82274ec2a7bf7f282b7b4e9d0fd32
Melvin Carvalho <melvincarvalho@gmail.com>
2019-06-14 23:48:30 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import Literal from './components/Literal.jsx'
+import NamedNode from './components/NamedNode.jsx'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <h1> Hello, World! </h1>
-        <Literal value='liter' />
+        <NamedNode value='https://www.scrolller.com/media/20f58d.jpg' />
       </div>
     )
   }
