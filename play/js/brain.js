78cd69e5a1e742977ef927a040d8baef8024b6e1
Melvin Carvalho <melvincarvalho@gmail.com>
2019-06-30 09:50:57 +0200

    cleanup

--- play/js/brain.js
+++ play/js/brain.js
@@ -7,10 +7,10 @@ var OFFSET = 128
  * @returns sha256
  */
 async function sha256 (str) {
-  const buf = await crypto.subtle.digest('SHA-256', new TextEncoder('utf-8').encode(str))
+  var bytes = new TextEncoder('utf-8').encode(str)
+  const buf = await crypto.subtle.digest('SHA-256', bytes)
   return Array.prototype.map.call(new Uint8Array(buf), x => (('00' + x.toString(16)).slice(-2))).join('')
 }
-
 
 /**
  * hex to bytes
@@ -26,7 +26,6 @@ function hexToBytes (str) {
   }
   return result
 }
-
 
 /**
  * get encoded point
