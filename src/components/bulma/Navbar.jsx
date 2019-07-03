2e3ce81fda894c1aa5a4e77b2d1ee69fafcb38fb
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-03 20:32:51 +0200

    add import

--- src/components/bulma/Navbar.jsx
+++ src/components/bulma/Navbar.jsx
@@ -1,4 +1,9 @@
 // import React, { Component } from 'react'
+if (0) {
+  var _react = _interopRequireWildcard(require("react"));
+
+  function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+}
 
 function NavbarAbout ({sourceCode, ...props}) {
   return <div className='navbar-item has-dropdown is-hoverable'>
@@ -150,3 +155,9 @@ Navbar.Container = NavbarContainer
 Navbar.Example = NavbarExample
 
 // export default NavbarSolid
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = NavbarSolid;
+exports.default = _default;
