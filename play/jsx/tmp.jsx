df88f639e50948c8f40ca7d545b6aa0e0bb2665c
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-03 23:24:51 +0200

    clean up tmp.jsx

---
+++ play/jsx/tmp.jsx
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  <NavbarSolid className="is-link" title="Tmp" sourceCode="https://github.com/play-grounds/react/blob/gh-pages/play/tmp.html"></NavbarSolid> ,
+  document.getElementById('root')
+);
