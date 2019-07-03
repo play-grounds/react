9bd74e62e8ef6f8d0210bdb2b49bb450716c4dfb
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-03 17:50:12 +0200

    cleanup

--- src/components/AddressBar.jsx
+++ src/components/AddressBar.jsx
@@ -1,24 +1,27 @@
+// import React from 'react'
+
 class AddressBar extends React.Component {
   constructor (props) {
     super(props)
-    var uri = subject
-    this.state = { uri: subject }
+    if (subject) {
+      this.state = { subject : subject }
+    }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange (event) {
-    this.setState({uri: event.target.value })
-    history.pushState({}, 'Bookmark App', window.location.href.split('?')[0] + '?uri=' 
-    + encodeURIComponent(event.target.value))
-    setSubject(event.target.value)
-    console.log('this.state.uri', this.state.uri)
+    this.setState({subject: event.target.value })
+    history.pushState({}, 'Bookmark App', window.location.href.split('?')[0] + '?uri=' + encodeURIComponent(event.target.value))
+    if (setSubject) {
+      setSubject(event.target.value)
+    }
   }
 
   render () {
     return (<div>
       <label>Address</label>
       <br/> 
-      <input size='80' onChange={this.handleChange} placeholder='uri' value={this.state.uri} />
+      <input size='80' onChange={this.handleChange} placeholder='uri' value={this.state.subject} />
       <hr />
 
       {this.props.children}
