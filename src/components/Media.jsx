d2469b7281c9932592263f27924bbdcb7ccfe6ba
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-05 20:01:42 +0200

    add mkv

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -6,7 +6,7 @@ class Media extends React.Component {
   }
   render() { 
     const AUDIO_EXTENSIONS = /\.(m4a|mp4a|mpga|mp2|mp2a|mp3|m2a|m3a|wav|weba|aac|oga|spx)($|\?)/i
-    const VIDEO_EXTENSIONS = /\.(mp4|og[gv]|webm|mov|m4v)($|\?)/i
+    const VIDEO_EXTENSIONS = /\.(mp4|og[gv]|webm|mov|m4v|mkv)($|\?)/i
     const IMAGE_EXTENSIONS = /\.(png|gif|bmp|svg|jpeg|jpg)($|\?)/i
 
 
