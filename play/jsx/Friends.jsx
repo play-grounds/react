ec3345f3a93384d56ebdb885802548010b1a8806
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-05 17:07:35 +0200

    standardjs

--- play/jsx/Friends.jsx
+++ play/jsx/Friends.jsx
@@ -4,82 +4,76 @@ UI.store = $rdf.graph()
 UI.fetcher = new $rdf.Fetcher(UI.store)
 UI.updater = new $rdf.UpdateManager(UI.store)
 
-var defaultUri = "https://melvincarvalho.com/#me"
-var subject = getQueryStringParam("uri") || defaultUri
-
+var defaultUri = 'https://melvincarvalho.com/#me'
+var subject = getQueryStringParam('uri') || defaultUri
 
 class FriendSet extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
-    this.state = { subject : subject }
+    this.state = { subject: subject }
   }
 
-  fetchFriends(subject) {
+  fetchFriends (subject) {
     UI.fetcher.load(subject).then(response => {
-
       let s = UI.store.sym(subject)
-      let p = UI.store.sym("http://xmlns.com/foaf/0.1/knows")
+      let p = UI.store.sym('http://xmlns.com/foaf/0.1/knows')
       let o = null
       let w = UI.store.sym(subject.split('#')[0])
-      let quads = UI.store.statementsMatching(s,p,o,w)
+      let quads = UI.store.statementsMatching(s, p, o, w)
       this.setState({ quads: quads })
-  
     }, err => {
       console.log(err)
-    })  
+    })
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.fetchFriends(this.props.subject)
   }
 
-  componentWillReceiveProps(props, current_state) {
+  componentWillReceiveProps (props, current_state) {
     this.fetchFriends(props.subject)
   }
 
-
-  render() {
-
+  render () {
     return (
       <div>
         {this.props.subject}
-        <hr/>
+        <hr />
         <NamedNodeSet nodes={this.state.quads} subject={this.props.subject} />
       </div>
     )
   }
 }
 
-
-function Body(props) {
+function Body (props) {
   return (
     <div>
 
       <div>
-        <section className="section">
+        <section className='section'>
           <Addressbar subject={subject}>
-          <FriendSet />
-          </Addressbar> 
+            <FriendSet />
+          </Addressbar>
         </section>
 
       </div>
 
     </div>
-  );
+  )
 }
 
-function App() {
+function App () {
   return (
     <div>
-      <NavbarSolid 
-        className="is-link" 
-        title="Friends App" 
-        sourceCode="https://github.com/play-grounds/react/blob/gh-pages/play/friends.html/" />
+      <NavbarSolid
+        className='is-link'
+        title='Friends App'
+        sourceCode='https://github.com/play-grounds/react/blob/gh-pages/play/friends.html/' />
       <Body />
 
     </div>
-  );
+  )
 }
 
 ReactDOM.render(
