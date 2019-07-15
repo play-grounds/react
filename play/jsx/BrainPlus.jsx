0c5190f27901d5f10a8d90c44ed92a861745b876
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-15 16:55:48 +0200

    cleanup

--- play/jsx/BrainPlus.jsx
+++ play/jsx/BrainPlus.jsx
@@ -235,7 +235,7 @@ class Body extends React.Component {
       localStorage.setItem('suffix', event.target.value)
     }
 
-    // @TODO add caching
+    // @TODO add caching 
     var uncompressed = await getKeyPairFromPW(
       combined,
       'uncompressed',
