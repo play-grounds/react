1f882a8d8556c66d172a06c55b0062349b3dfe7c
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-16 19:00:14 +0200

    add auto refresh

--- play/jsx/Solid.jsx
+++ play/jsx/Solid.jsx
@@ -4,6 +4,23 @@ UI.store = $rdf.graph()
 UI.fetcher = new $rdf.Fetcher(UI.store)
 UI.updater = new $rdf.UpdateManager(UI.store)
 
+// refresh on change
+let uri = location.href
+let wss = uri.replace('http', 'ws')
+let touchUri = uri
+let w = new WebSocket(wss)
+w.onmessage = function (m) {
+  let data = m.data
+  console.log('data', data)
+  if (data.match(/pub .*/)) {
+    location.reload()
+  }
+}
+w.onopen = function () {
+  w.send('sub ' + touchUri)
+}
+
+
 var subject = getQueryStringParam('uri') || 'https://melvincarvalho.com/#me'
 
 class Solid extends React.Component {
