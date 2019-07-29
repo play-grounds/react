f4b29d0aa7dfc3032d0923181d7d5b2623b2ac8f
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-29 14:56:55 +0200

    load bookmark if rdf

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -10,13 +10,21 @@ class Bookmark extends React.Component {
   constructor (props) {
     super(props)
     let media = this.isMedia()
-    this.state = { 'media' : media }
+    this.state = { 'media' : media, 'subject' : props.subject }
   }
 
   componentDidMount() {
     if (this.state.media === false) {
       console.log('fetch bookmark')
-    }        
+      UI.fetcher.load(subject).then(response => {
+        let s = UI.store.sym(this.props.subject)
+        let p = UI.store.sym('http://www.w3.org/2002/01/bookmark#recalls')
+        let o = null
+        let w = UI.store.sym(subject.split('#')[0])
+        let recalls = UI.store.any(s, p, o, w)
+        this.setState('subject' : recalls)
+      })
+    }
   }
   
   isMedia() {
@@ -31,11 +39,11 @@ class Bookmark extends React.Component {
   render () {
     if (this.state.media) {
       return (
-        <Media href={this.props.subject} />
+        <Media href={this.state.subject} />
       )  
     } else {
       return (
-        <Media href={this.props.subject} />
+        <Media href={this.state.subject} />
       )        
     }
   }
