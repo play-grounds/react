05a3e417b540e6cf33334f08ac9bffd77f74956c
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-29 13:32:00 +0200

    add rdflib globals

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,16 +1,34 @@
 //REMOVE import React from 'react'
 //REMOVE import Media from './Media.jsx'
 
+var UI = {}
+UI.store = $rdf.graph()
+UI.fetcher = new $rdf.Fetcher(UI.store)
+UI.updater = new $rdf.UpdateManager(UI.store)
+
 class Bookmark extends React.Component {
   constructor (props) {
     super(props)
   }
+  isMedia() {
+    // TODO better test for linked data
+    if (this.props.subject.match(/#/)) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   render () {
-    let isMedia = true
-    if (isMedia) {
+    let media = this.isMedia()
+    if (media) {
       return (
         <Media href={this.props.subject} />
       )  
+    } else {
+      return (
+        <Media href={this.props.subject} />
+      )        
     }
   }
 }
