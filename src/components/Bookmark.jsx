fbd9b07d6f941cd62debb63cf70941222b4add9b
Melvin Carvalho <melvincarvalho@gmail.com>
2019-07-29 23:52:31 +0200

    lists of bookmarks

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -6,29 +6,78 @@ UI.store = $rdf.graph()
 UI.fetcher = new $rdf.Fetcher(UI.store)
 UI.updater = new $rdf.UpdateManager(UI.store)
 
+
+function BookmarkItem(props) {
+  return (
+    <a target="_blank" href={props.recalls}>{props.title}</a>
+  )
+}
+
+function getTypeFromSubject(subject) {
+  let s = UI.store.sym(subject)
+  let p = UI.store.sym('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+  let o = null
+  let w = UI.store.sym(subject.split('#')[0])
+  let type = UI.store.any(s, p, o, w)
+  if (type) {
+    return type.value
+  } else {
+    return null
+  }
+}
+
 class Bookmark extends React.Component {
   constructor (props) {
     super(props)
     let media = this.isMedia(props.subject)
-    this.state = { 'media' : media, 'subject' : props.subject, 'title' : '' }
+    this.state = { 'media' : media, 'subject' : props.subject, 'title' : '', bookmark : [ { 'recalls' : '', 'title' : '' } ] }
   }
 
   fetchBookmark (subject) {
     console.log('fetch bookmark', subject)
     UI.fetcher.load(subject).then(response => {
-      let s = UI.store.sym(subject)
-      let p = UI.store.sym('http://www.w3.org/2002/01/bookmark#recalls')
-      let o = null
-      let w = UI.store.sym(subject.split('#')[0])
-      let recalls = UI.store.any(s, p, o, w)
-      s = UI.store.sym(subject)
-      p = UI.store.sym('http://purl.org/dc/terms/title')
-      o = null
-      w = UI.store.sym(subject.split('#')[0])
-      let title = UI.store.any(s, p, o, w)
-      console.log(recalls);
+      var type = getTypeFromSubject(subject)
+      var bm = []
+      console.log('type', type);
       
-      this.setState({'recalls' : recalls.value, 'title' : title.value })
+      if (!type) {
+        let s = UI.store.sym(subject)
+        let p = UI.store.sym('http://purl.org/dc/terms/references')
+        let subjects = UI.store.statementsMatching(s, p)
+        for (let subject of subjects) {
+          subject = subject.object.value
+          let s = UI.store.sym(subject)
+          let p = UI.store.sym('http://www.w3.org/2002/01/bookmark#recalls')
+          let o = null
+          let w = UI.store.sym(subject.split('#')[0])
+          let recalls = UI.store.any(s, p, o, w)
+          s = UI.store.sym(subject)
+          p = UI.store.sym('http://purl.org/dc/terms/title')
+          o = null
+          w = UI.store.sym(subject.split('#')[0])
+          let title = UI.store.any(s, p, o, w)
+          let bookmark = { 'recalls' : recalls.value, 'title' : title.value }
+          bm.push(bookmark)
+        }
+
+        this.setState({'bookmark' : bm })  
+
+        console.log('bm', bm); 
+
+      } else {
+        let s = UI.store.sym(subject)
+        let p = UI.store.sym('http://www.w3.org/2002/01/bookmark#recalls')
+        let o = null
+        let w = UI.store.sym(subject.split('#')[0])
+        let recalls = UI.store.any(s, p, o, w)
+        s = UI.store.sym(subject)
+        p = UI.store.sym('http://purl.org/dc/terms/title')
+        o = null
+        w = UI.store.sym(subject.split('#')[0])
+        let title = UI.store.any(s, p, o, w)
+        let bookmark = [{ 'recalls' : recalls.value, 'title' : title.value }]
+        this.setState({'recalls' : recalls.value, 'title' : title.value, 'bookmark' : bookmark })  
+      }
     }, err => {
       console.log(err)
     })
@@ -51,7 +100,7 @@ class Bookmark extends React.Component {
   isMedia(subject) {
     // TODO better test for linked data
     let isMedia = false
-    if (subject.match(/#/)) {
+    if (subject.match(/.ttl/)) {
       isMedia = false
     } else {
       isMedia = true
@@ -68,11 +117,21 @@ class Bookmark extends React.Component {
         <Media href={this.props.subject} />
       )  
     } else {
+      const listItems = this.state.bookmark.map((b) =>
+      // Wrong! The key should have been specified here:
+      <div>
+        <BookmarkItem recalls={b.recalls} title={b.title} />
+      </div>
+
+      );
+
       return (
-        <a target="_blank" href={this.state.recalls}>{this.state.title}</a>
+        <div>{listItems}</div>
       )        
     }
   }
 }
+
+
 
 //REMOVE export default Bookmark
