a833e3041dccc26cc5f2433ddf0e92f406cbeb86
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-03 10:56:15 +0200

    add avatar

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -111,11 +111,33 @@ class Person extends React.Component {
 
   render() {
     let name = this.state.profile ? this.state.profile.name : 'has no name'
+    let profile = this.state.profile || {}
+    let avatar = getAvatarFromSubject(this.state.subject) || 'https://bulma.io/images/placeholders/1280x960.png'
     console.log('this.state.profile', this.state.profile)
     return (
-        <div>
-          {name}
-        </div>
+
+
+<div className="card">
+  <div className="card-image">
+    <figure className="image is-4by3">
+      <img src={avatar} alt="Placeholder image" />
+    </figure>
+  </div>
+  <div className="card-content">
+    <div className="media">
+      <div className="media-content">
+        <p className="title is-4">{name}</p>
+        <p className="subtitle is-6">@{profile.nick}</p>
+      </div>
+    </div>
+
+    <div className="content">
+      <a href="#">#solid</a> <a href="#">#profile</a>
+      <br/>
+      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    </div>
+  </div>
+</div>
     )
   }
 }
