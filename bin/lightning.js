e4a834faed73990b30466742a27530f87856834a
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-18 12:03:51 +0200

    add ssl and user auth to server

--- bin/lightning.js
+++ bin/lightning.js
@@ -1,10 +1,14 @@
+const webid = require('webid')('tls')
+const https = require('https')
 const fs = require('fs')
 const express = require('express')
 const bodyParser = require('body-parser')
 const lnService = require('ln-service')
 
+// init
 const port = 8888
 
+// get creds
 const macaroonPath =
   process.env.HOME + '/.lnd/data/chain/bitcoin/mainnet/admin.macaroon'
 const tlsPath = process.env.HOME + '/.lnd/tls.cert'
@@ -12,125 +16,187 @@ const tlsPath = process.env.HOME + '/.lnd/tls.cert'
 const macaroon = fs.readFileSync(macaroonPath).toString('base64')
 const tls = fs.readFileSync(tlsPath).toString('base64')
 
+// get ln service
 const { lnd } = lnService.authenticatedLndGrpc({
   cert: tls,
   macaroon: macaroon,
   socket: '127.0.0.1:10009'
 })
 
-// var balance = 2000
 var amount = 999
 
+/**
+ * getBalance
+ *
+ * @param {string} webId of theuser
+ */
 function getBalance (user) {
   return ledger[user]
 }
 
+// get ledger
 var ledger = require('./ledger.json')
 var user = process.env.USER || 'https://melvincarvalho.com/#me'
-
 var balance = getBalance(user)
+var webId = null
 console.log('ledger', ledger)
 
-const app = express()
+var options = {
+  key: fs.readFileSync('privkey.pem'),
+  cert: fs.readFileSync('fullchain.pem'),
+  requestCert: true,
+  rejectUnauthorized: false
+}
+
+var app = express()
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 )
 
-app.listen(port, function () {
-  console.log(`Solidpay server listening on port http://localhost:${port}`)
-  app.get('/channel', (req, res) => {
-    lnService.getChannelBalance({ lnd }, (err, result) => {
-      res.send('<pre>channel balance is \n' + result.channel_balance)
-    })
+app.use(function (req, res, next) {
+  if (!req.client) {
+    return res.status(401).send('No client cert')
+  }
+
+  var cert = req.socket.getPeerCertificate()
+  console.log(cert)
+  webid.verify(cert, (err, res) => {
+    console.log('webid', res)
+    user = res
   })
 
-  app.get('/info', (req, res) => {
-    // Callback syntax
-    lnService.getWalletInfo({ lnd }, (err, result) => {
-      res.send('<pre>public key is : \n' + result.public_key)
-    })
-  })
+  next()
+})
 
-  app.get('/balance', (req, res) => {
-    // Callback syntax
-    res.send(`<pre>user : ${user}\nbalance : ${balance}`)
+app.get('/channel', (req, res) => {
+  lnService.getChannelBalance({ lnd }, (err, result) => {
+    res.send('<pre>channel balance is \n' + result.channel_balance)
   })
+})
 
-  app.get('/index', (req, res) => {
-    // Callback syntax
-    var output = `Balance : <a href="/balance">Balance</a><br />`
-    output += `Pay : <a href="/pay">Pay</a><br />`
-    res.send(`${output}`)
+// info
+app.get('/info', (req, res) => {
+  lnService.getWalletInfo({ lnd }, (err, result) => {
+    res.send('<pre>public key is : \n' + result.public_key)
   })
+})
 
-  app.get('/pay', (req, res) => {
-    // Callback syntax
-    const request = req.query.request
-    console.log('request', request)
-    if (!request) {
-      res.send('add a payment request')
+// balance
+app.get('/balance', (req, res) => {
+  res.send(`<pre>user : ${user}\nbalance : ${balance}`)
+})
+
+// index
+app.get('/index', (req, res) => {
+  var output = `Balance : <a href="/balance">Balance</a><br />`
+  output += `Pay : <a href="/pay">Pay</a><br />`
+  res.send(`${output}`)
+})
+
+// pay with get -- dangerous
+// app.get('/pay', (req, res) => {
+//   const request = req.query.request
+//   console.log('request', request)
+//   if (!request) {
+//     res.send('add a payment request')
+//     return
+//   }
+//   lnService.decodePaymentRequest({ lnd, request: request }, (err, result) => {
+//     console.log('decoded', result)
+//     amount = result.tokens
+//     if (amount > ledger[user]) {
+//       console.log('not enough funds')
+//       res.send('not enough funds')
+//       return
+//     }
+//     ledger[user] -= amount
+//     console.log('ledger', ledger)
+//     // @@ not concurrent for now
+//     fs.writeFileSync('./ledger.json', JSON.stringify(ledger))
+//     lnService.payViaPaymentRequest({ lnd, request: request }, (err, result) => {
+//       res.send(
+//         `<pre>request = ${request} \n\n${result ? result.toString() : ''}\n\n${
+//           err ? err.toString() : ''
+//         }`
+//       )
+//     })
+//   })
+// })
+
+// pay with post
+app.post('/pay', (req, res) => {
+  // console.log('request', req.body)
+  const request = req.body.request
+  console.log('req.body', req.body)
+  console.log('request', request)
+  if (!request) {
+    console.log('no request found')
+    res.send('add a payment request')
+    return
+  }
+  console.log('request', request)
+  lnService.decodePaymentRequest({ lnd, request: request }, (err, result) => {
+    console.log('decoded', result)
+    amount = result.tokens
+    if (amount > ledger[user]) {
+      console.log('not enough funds')
+      res.send('not enough funds')
       return
     }
-    lnService.decodePaymentRequest({ lnd, request: request }, (err, result) => {
-      console.log('decoded', result)
-      amount = result.tokens
-      if (amount > ledger[user]) {
-        console.log('not enough funds')
-        res.send('not enough funds')
-        return
-      }
-      ledger[user] -= amount
-      console.log('ledger', ledger)
-      // @@ not concurrent for now
-      fs.writeFileSync('./ledger.json', JSON.stringify(ledger))
-      lnService.payViaPaymentRequest(
-        { lnd, request: request },
-        (err, result) => {
-          res.send(
-            `<pre>request = ${request} \n\n${
-              result ? result.toString() : ''
-            }\n\n${err ? err.toString() : ''}`
-          )
-        }
+    ledger[user] -= amount
+    console.log('ledger', ledger)
+    // @@ not concurrent for now
+    fs.writeFileSync('./ledger.json', JSON.stringify(ledger))
+    lnService.payViaPaymentRequest({ lnd, request: request }, (err, result) => {
+      res.send(
+        `<pre>request = ${request} \n\n${result ? result.toString() : ''}\n\n${
+          err ? err.toString() : ''
+        }`
       )
     })
   })
-  app.post('/pay', (req, res) => {
-    // Callback syntax
-    // console.log('request', req.body)
-    const request = req.body.request
-    console.log('req.body', req.body)
-    console.log('request', request)
-    if (!request) {
-      console.log('no request found')
-      res.send('add a payment request')
+})
+
+// pay with post
+app.get('/pay', (req, res) => {
+  // console.log('request', req.body)
+  const request = req.body.request
+  console.log('req.body', req.body)
+  console.log('request', request)
+  if (!request) {
+    console.log('no request found')
+    res.send('add a payment request')
+    return
+  }
+  console.log('request', request)
+  lnService.decodePaymentRequest({ lnd, request: request }, (err, result) => {
+    console.log('decoded', result)
+    amount = result.tokens
+    if (amount > ledger[user]) {
+      console.log('not enough funds')
+      res.send('not enough funds')
       return
     }
-    console.log('request', request)
-    lnService.decodePaymentRequest({ lnd, request: request }, (err, result) => {
-      console.log('decoded', result)
-      amount = result.tokens
-      if (amount > ledger[user]) {
-        console.log('not enough funds')
-        res.send('not enough funds')
-        return
-      }
-      ledger[user] -= amount
-      console.log('ledger', ledger)
-      // @@ not concurrent for now
-      fs.writeFileSync('./ledger.json', JSON.stringify(ledger))
-      lnService.payViaPaymentRequest(
-        { lnd, request: request },
-        (err, result) => {
-          res.send(
-            `<pre>request = ${request} \n\n${
-              result ? result.toString() : ''
-            }\n\n${err ? err.toString() : ''}`
-          )
-        }
+    ledger[user] -= amount
+    console.log('ledger', ledger)
+    // @@ not concurrent for now
+    fs.writeFileSync('./ledger.json', JSON.stringify(ledger))
+    lnService.payViaPaymentRequest({ lnd, request: request }, (err, result) => {
+      res.send(
+        `<pre>request = ${request} \n\n${result ? result.toString() : ''}\n\n${
+          err ? err.toString() : ''
+        }`
       )
     })
   })
+})
+
+app.get('/', function (req, res) {
+  res.send('hello worlds')
+})
+
+https.createServer(options, app).listen(port, function () {
+  console.log('Solidpay server running at : https://localhost:' + port + '/')
 })
