9e21c38109800e7e4f00fe2c98023355a400f677
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-28 13:17:44 +0200

    improve acl

--- play/jsx/ACL.jsx
+++ play/jsx/ACL.jsx
@@ -4,125 +4,54 @@ UI.store = $rdf.graph()
 UI.store.fetcher = new $rdf.Fetcher(UI.store)
 UI.updater = new $rdf.UpdateManager(UI.store)
 
-var subject = new URLSearchParams(document.location.search).get('uri')
-  || 'https://melvin.solid.live/public/timeline/'
-var view = new URLSearchParams(document.location.search).get('view') || 'default'
-
-
-window.onpopstate = function (event) {
-  console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
-  location.reload()
-};
-
-function fetchResource(subject) {
-  UI.store.fetcher.load(subject).then(response => {
-  }, err => {
-    console.log(err)
-  })
-}
-
-class Resource extends React.Component {
-  constructor(props) {
-    super(props)
-    let view = new URLSearchParams(document.location.search).get('view')
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  componentDidMount() {
-    UI.store.fetcher.load(UI.store.sym(this.props.subject)).then(response => {
-      this.setState({ subject: this.props.subject })
-    }, err => {
-      console.log(err)
-    })
-  }
-
-  handleClick(event) {
-  }
-
-
-  render() {
-    return (
-      <div><a target="_blank" href={this.props.name}>{this.props.name}</a></div>
-    )
-  }
-}
-
 class ACL extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
-
-    this.state = { subject: subject }
   }
 
-  fetchCointainer(subject) {
-    UI.store.fetcher.load(subject).then(response => {
-      let s = UI.store.sym(subject)
-      let p = UI.store.sym('http://www.w3.org/ns/ldp#contains')
-      let o = null
-      let w = UI.store.sym(subject.split('#')[0])
-      let quads = UI.store.statementsMatching(s, p, o, w)
-      console.log(quads)
-      this.setState({ quads: quads })
-    }, err => {
-      console.log(err)
-    })
-  }
+  componentDidMount () {}
 
-  componentDidMount() {
-    this.fetchCointainer(this.props.subject)
-  }
-
-  componentWillReceiveProps(props) {
-    this.fetchCointainer(props.subject)
-  }
-
-  render() {
-    if (this.state.quads) {
-      var friendSet = this.state.quads.map((quad) => {
-        return <Resource subject={quad.object.value} name={quad.object.value} />
-      })
-    }
-
+  render () {
     return (
       <div>
-        <ul>{friendSet}</ul>
+        <ul>ACL</ul>
       </div>
     )
   }
 }
 
-function Body(props) {
+function Body (props) {
+  var aclpub = `@prefix   acl:  <http://www.w3.org/ns/auth/acl#>.
+  @prefix  foaf:  <http://xmlns.com/foaf/0.1/>.
+  
+  <#authorization2>
+      a               acl:Authorization;
+      acl:agentClass  foaf:Agent;                               # everyone
+      acl:mode        acl:Read;                                 # has Read-only access
+      acl:accessTo    <https://alice.databox.me/profile/card>.  # to the public profile`
   return (
     <div>
-
       <div>
         <section className='section'>
-          <AddressBar subject={subject}
-            view={new URLSearchParams(document.location.search).get('view')}>
-            <ACL />
-          </AddressBar>
+          <label>Public</label>
+          <pre>{aclpub}</pre>
         </section>
-
       </div>
-
     </div>
   )
 }
 
-function App() {
+function App () {
   return (
     <div>
       <NavbarSolidLogin
         className='is-link'
         title='ACL App'
-        sourceCode='https://github.com/play-grounds/react/blob/gh-pages/play/ACL.html/' />
+        sourceCode='https://github.com/play-grounds/react/blob/gh-pages/play/acl.html/'
+      />
       <Body />
-
     </div>
   )
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
