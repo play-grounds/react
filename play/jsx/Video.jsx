41aba5bb14f96bb6522106c1c011eb946d2eb2c9
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-29 09:07:04 +0200

    add video

---
+++ play/jsx/Video.jsx
@@ -0,0 +1,11 @@
+ReactDOM.render(
+  <div>
+    <NavbarSolid
+      className='is-link'
+      title='Video'
+      sourceCode='https://github.com/play-grounds/react/blob/gh-pages/play/video.html'
+    />
+  </div>,
+
+  document.getElementById('root')
+)
