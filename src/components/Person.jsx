695aa4984aa7286f277600f5718e9e6ffba3a44c
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-29 11:21:07 +0200

    add icons to person

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,19 +1,18 @@
-//REMOVE import React from 'react' ; import Person from './Person.jsx'
+// REMOVE import React from 'react' ; import Person from './Person.jsx'
 
 var UI = {}
 UI.store = $rdf.graph()
 UI.fetcher = new $rdf.Fetcher(UI.store)
 UI.updater = new $rdf.UpdateManager(UI.store)
 
-const SIOC = $rdf.Namespace("http://rdfs.org/sioc/ns#")
+const SIOC = $rdf.Namespace('http://rdfs.org/sioc/ns#')
 const DCT = $rdf.Namespace('http://purl.org/dc/terms/')
 const RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/')
 const VCARD = $rdf.Namespace('http://www.w3.org/2006/vcard/ns#')
 const SOLID = $rdf.Namespace('http://www.w3.org/ns/solid/terms#')
 
-function getVal(subject, predicate) {
-
+function getVal (subject, predicate) {
   if (!subject || !predicate) return
   let s = UI.store.sym(subject)
   let p = predicate
@@ -27,11 +26,11 @@ function getVal(subject, predicate) {
   }
 }
 
-function getTypeFromSubject(subject) {
+function getTypeFromSubject (subject) {
   return getVal(subject, RDF('type'))
 }
 
-function getProfileFromSubject(subject) {
+function getProfileFromSubject (subject) {
   var profile = {}
   profile.type = getVal(subject, RDF('type'))
   profile.name = getVal(subject, FOAF('name'))
@@ -49,49 +48,49 @@ function getProfileFromSubject(subject) {
   return profile
 }
 
-
-function getNameFromSubject(subject) {
+function getNameFromSubject (subject) {
   let profile = getProfileFromSubject(subject)
   let name = profile.name || profile.nick || profile.fn || subject
   return name
 }
 
-function getNickFromSubject(subject) {
+function getNickFromSubject (subject) {
   let profile = getProfileFromSubject(subject)
   let nick = profile.nickname || profile.nick
   return nick
 }
 
-function getAvatarFromSubject(subject) {
+function getAvatarFromSubject (subject) {
   let profile = getProfileFromSubject(subject)
-  let avatar = profile.img || profile.image || profile.depiction || profile.hasPhoto
+  let avatar =
+    profile.img || profile.image || profile.depiction || profile.hasPhoto
   return avatar
 }
 
 class Person extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
-    this.state = { 'subject': props.subject }
+    this.state = { subject: props.subject }
   }
 
-  fetchProfile(subject) {
-    console.log('fetchProfile', subject);
+  fetchProfile (subject) {
+    console.log('fetchProfile', subject)
     if (!subject) return
     if (subject.match(/reddit.com.*this$/)) return
 
     UI.fetcher.load(subject).then(response => {
       console.log('fetched', subject)
-      this.setState({ 'profile': getProfileFromSubject(subject) })
+      this.setState({ profile: getProfileFromSubject(subject) })
     })
   }
 
-  getUpdatesVia(doc) {
+  getUpdatesVia (doc) {
     var linkHeaders = UI.store.fetcher.getHeader(doc, 'updates-via')
     if (!linkHeaders || !linkHeaders.length) return null
     return linkHeaders[0].trim()
   }
 
-  setRefreshHandler(subject, handler) {
+  setRefreshHandler (subject, handler) {
     var self = this
     var wss = this.getUpdatesVia(subject)
     let w = new WebSocket(wss)
@@ -107,59 +106,92 @@ class Person extends React.Component {
     }
   }
 
-  refresh() {
+  refresh () {
     this.fetchProfile(this.state.subject)
   }
 
-  componentDidMount() {
+  componentDidMount () {
     let subject = this.state.subject
     this.fetchProfile(subject)
   }
 
-  componentWillReceiveProps(props) {
+  componentWillReceiveProps (props) {
     let subject = props.subject
 
     this.fetchProfile(subject)
   }
 
-  render() {
+  render () {
     let profile = this.state.profile || {}
-    let name = this.state.profile ? getNameFromSubject(this.state.subject) : 'has no name'
-    let avatar = getAvatarFromSubject(this.state.subject) || 'https://bulma.io/images/placeholders/1280x960.png'
+    let name = this.state.profile
+      ? getNameFromSubject(this.state.subject)
+      : 'has no name'
+    let avatar =
+      getAvatarFromSubject(this.state.subject) ||
+      'https://bulma.io/images/placeholders/1280x960.png'
     let nick = getNickFromSubject(this.state.subject) || ''
     console.log('this.state.profile', this.state.profile)
     return (
-
-
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img src={avatar} alt="Placeholder image" />
+      <div className='card'>
+        <div className='card-image'>
+          <figure className='image is-4by3'>
+            <img src={avatar} alt='Placeholder image' />
           </figure>
         </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-4">{name}</p>
-              <p className="subtitle is-6">@{nick}</p>
+        <div className='card-content'>
+          <div className='media'>
+            <div className='media-content'>
+              <p className='title is-4'>{name}</p>
+              <p className='subtitle is-6'>@{nick}</p>
             </div>
           </div>
 
-          <div className="content">
-            <a href="#">#solid</a> <a href="#">#profile</a>
+          <div className='content'>
+            <a href='#'>#solid</a> <a href='#'>#profile</a>
             <br />
           </div>
 
-          <div className="content">
-            <a target="_blank" href={'./timeline.html?uri=' + profile.timeline}>Timeline</a>
+          <div className='content'>
+            <a target='_blank' href={'./timeline.html?uri=' + profile.timeline}>
+              Timeline
+            </a>
             <br />
           </div>
-
-
-
         </div>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+        />
+
+        <ul>
+          <li>
+            <a href='https://www.facebook.com'>
+              <i className='fa fa-facebook' aria-hidden='true' />
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/'>
+              <i className='fa fa-twitter' aria-hidden='true' />
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <i className='fa fa-google-plus' aria-hidden='true' />
+            </a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/'>
+              <i className='fa fa-linkedin' aria-hidden='true' />
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <i className='fa fa-instagram' aria-hidden='true' />
+            </a>
+          </li>
+        </ul>
       </div>
     )
   }
 }
-//REMOVE export default Person
+// REMOVE export default Person
