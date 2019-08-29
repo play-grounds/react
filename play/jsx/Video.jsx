9f59059a0253d6a6162a7fb27a0b320ec3e7b335
Melvin Carvalho <melvincarvalho@gmail.com>
2019-08-29 09:15:52 +0200

    add youtube as search param

--- play/jsx/Video.jsx
+++ play/jsx/Video.jsx
@@ -1,3 +1,25 @@
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    let youtube =
+      new URLSearchParams(document.location.search.get('youtube')) ||
+      'bTqVqk7FSmY'
+    this.setState({ youtube: youtube })
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        <div
+          id='player'
+          data-plyr-provider='youtube'
+          data-plyr-embed-id={this.state.youtube}
+        />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <div>
     <NavbarSolid
@@ -5,7 +27,21 @@ ReactDOM.render(
       title='Video'
       sourceCode='https://github.com/play-grounds/react/blob/gh-pages/play/video.html'
     />
+
+    <div className='container'>
+      <div
+        id='player'
+        data-plyr-provider='youtube'
+        data-plyr-embed-id='bTqVqk7FSmY'
+      />
+    </div>
   </div>,
 
   document.getElementById('root')
 )
+
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('#player', {})
+
+// Expose player so it can be used from the console
+window.player = player
