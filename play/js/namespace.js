afba564bb82c21ef72d65228ef4275236eb91f3c
Melvin Carvalho <melvincarvalho@gmail.com>
2019-09-06 16:30:01 +0200

    add currency to namespaces

--- play/js/namespace.js
+++ play/js/namespace.js
@@ -1,5 +1,6 @@
 var AS = $rdf.Namespace('https://www.w3.org/ns/activitystreams#')
 var BOOK = $rdf.Namespace('http://www.w3.org/2002/01/bookmark#')
+var CURR = $rdf.Namespace('httpa://w3id.org/cc#')
 var DCT = $rdf.Namespace('http://purl.org/dc/terms/')
 var FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/')
 var RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
