0b6095c9e3fe0563624aebe025ba1e485bebafab
Melvin Carvalho <melvincarvalho@gmail.com>
2020-04-21 21:19:15 +0200

    add cycle text

--- play/jsx/Points.jsx
+++ play/jsx/Points.jsx
@@ -283,7 +283,9 @@ function Points () {
       <hr />
 
       <div className='buttons'>
-        <span className='button is-large is-warning'>Sprint : {c}</span>
+        <span className='button is-large is-warning'>
+          Cycle : {Math.floor(c / 30) + '/' + Math.floor(s / 5)}
+        </span>
         {/* <span className='button is-large is-success'>C : {l}</span> */}
         <span className='button is-large is-info'>Hour : {t}</span>
         <span className='button is-large is-link'>Day : {d}</span>
